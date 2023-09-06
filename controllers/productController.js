afb907afd8efe6e4e4c7bfefdc8654565a1c1d91
Jerry Wang <jerry.wang2352@gmail.com>
2023-09-06 04:29:49 -0400

    Add update and remove functionalities

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,7 @@ const Product = require('../models/productModel')
 const { getPostData } = require('../utils')
 
 //@desc Get All Products
-//@route GET /api/product/:id
+//@route GET /api/products
 async function getProducts(req,res) {
   try {
     const products = await Product.findAll() 
@@ -15,6 +15,8 @@ async function getProducts(req,res) {
   }
 }
 
+//@desc Get a Product
+//@route GET /api/products/:id
 async function getProduct(req,res,id) {
   try {
     const product = await Product.findById(id) 
@@ -57,8 +59,64 @@ async function createProduct(req,res) {
   }
 }
 
+//@desc Update a Product
+//@route PUT /api/products/:id
+async function updateProduct(req,res,id) {
+  try {
+    const product = await Product.findById(id)
+
+    if (!product) {
+      res.writeHead(404,{'Content-Type': 'application/json'})
+      res.end((JSON.stringify({message: 'Product Not Found'})))
+    } else{
+      const body = await getPostData(req)  
+      
+      const {title, description, price} = JSON.parse(body)
+      
+      const productData = {
+        title: title || product.title,
+        description: description || product.description,
+        price: price || product.price
+      }
+
+      const updProduct = await Product.update(id, productData) 
+
+      res.writeHead(200,{'Content-Type':'application/json'})
+      return res.end(JSON.stringify(updProduct))
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+//@desc Delete a Product
+//@route DELETE /api/products/:id
+async function deleteProduct(req,res,id) {
+  try {
+    const product = await Product.findById(id) 
+
+    if (!product) {
+      res.writeHead(404,{'Content-Type': 'application/json'})
+      res.end((JSON.stringify({message: 'Product Not Found'})))
+    } else {
+      await Product.remove(id)
+      res.writeHead(200,{'Content-Type': 'application/json'})
+      res.end((JSON.stringify({message:`Product ${id} removed`})))
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+
 module.exports = {
   getProducts,
   getProduct,
-  createProduct
+  createProduct,
+  updateProduct,
+  deleteProduct
 }
