aee1e4a1f40b382f86a544a6e1c8d86026c4eb1c
Jerry Wang <jerry.wang2352@gmail.com>
2023-09-06 03:52:01 -0400

    Implement POST functionality in API

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require('../models/productModel')
 
+const { getPostData } = require('../utils')
+
 //@desc Get All Products
 //@route GET /api/product/:id
 async function getProducts(req,res) {
@@ -30,7 +32,33 @@ async function getProduct(req,res,id) {
   }
 }
 
+
+//@desc Create a Product
+//@route POST /api/products
+async function createProduct(req,res) {
+  try {
+
+    const body = await getPostData(req)
+    const {title,description,price} = JSON.parse(body)
+      
+      const product = {
+        title,
+        description,
+        price,
+      }
+
+      const newProduct = await Product.create(product) 
+
+      res.writeHead(201,{'Content-Type':'application/json'})
+      return res.end(JSON.stringify(newProduct))
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 module.exports = {
   getProducts,
-  getProduct
+  getProduct,
+  createProduct
 }
